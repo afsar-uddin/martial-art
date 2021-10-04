ad0ac4a7e0679e55ef82ded9b625189d985afac7
afsar-uddin <afsarwebdev@gmail.com>
2021-10-04 08:58:26 +0600

    banner btn fixed

--- src/Components/Banner/BannerSlide.js
+++ src/Components/Banner/BannerSlide.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const BannerSlide = (props) => {
     const { title, cover, dsc } = props.bannerSlide
@@ -8,6 +9,7 @@ const BannerSlide = (props) => {
             <div className="banner-caption">
                 <h2>{title}</h2>
                 <p>{dsc}</p>
+                <Link className="btn-link" to="/">All Services</Link>
             </div>
         </div>
     );
