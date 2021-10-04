d183bca138be1259a56a8b5f169f6990e311a1dd
afsar-uddin <afsarwebdev@gmail.com>
2021-10-04 14:06:22 +0600

    footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import About from './Components/Aboutus/About';
 import Contact from './Components/Contact/Contact';
+import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
 import Home from './Components/Home/Home';
 import NotFound from './Components/NotFound/NotFound';
@@ -31,6 +32,7 @@ function App() {
             <NotFound></NotFound>
           </Route>
         </Switch>
+        <Footer></Footer>
       </Router>
     </div>
   );
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,12 +1,55 @@
 import React from 'react';
-import { Container, Row } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
+import { Link, NavLink } from 'react-router-dom';
+import Logo from '../../images/logo-transparent.png'
+import './Footer.css'
+import { FaFacebookF } from 'react-icons/fa';
+import { FaTwitter } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa';
+import { FaInstagramSquare } from 'react-icons/fa';
+
 
 const Footer = () => {
     return (
         <div className="footer">
             <Container>
-                <Row>
-                    Footer
+                <Row md={3} className="g-4">
+                    <Col>
+                        <div className="single-footer">
+                            <Link to="/">
+                                <img src={Logo} />
+                                <span>Banglar Martial Art</span>
+                            </Link>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div className="single-footer">
+                            <h3>Quick Link</h3>
+                            <nav>
+                                <NavLink exact to="/">Home</NavLink>
+                                <NavLink exact to="/servicesall">Services</NavLink>
+                                <NavLink exact to="/aboutus">About us</NavLink>
+                                <NavLink exact to="/contact">Contact us</NavLink>
+                            </nav>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div className="single-footer social">
+                            <h3>social link</h3>
+                            <Link to="#">
+                                <FaFacebookF />
+                            </Link>
+                            <Link to="#">
+                                <FaTwitter />
+                            </Link>
+                            <Link to="#">
+                                <FaLinkedin />
+                            </Link>
+                            <Link to="#">
+                                <FaInstagramSquare />
+                            </Link>
+                        </div>
+                    </Col>
                 </Row>
             </Container>
         </div>
